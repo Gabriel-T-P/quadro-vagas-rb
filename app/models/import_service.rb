85c357719f7f191e85cad57909d73335307db622
Gabriel-T-P <gabrielthiago2000@hotmail.com>
2025-03-15 16:09:32 -0300

    métdos do PORO import_service para processar as empresas e vagas de trabalhos criados e testados

--- app/models/import_service.rb
+++ app/models/import_service.rb
@@ -20,10 +20,8 @@ class ImportService
 
       case row[0]&.strip
       when "U" then process_user(row, line_number)
-      when "E"
-        # process_company(row, line_number)
-      when "V"
-        # process_job(row, line_number)
+      when "E" then process_company(row, line_number)
+      when "V" then process_job(row, line_number)
       else
         puts ("Linha ignorada: #{row}")
       end
@@ -60,8 +58,50 @@ class ImportService
   end
 
   def process_company(row, line_number)
+    name = row[1]&.strip
+    website_url = row[2]&.strip
+    email = row[3]&.strip
+    user_id = row[4]&.strip
+    logo = Rails.root.join("spec", "support", "files", "logo.png")
+
+    user = User.find_by(id: user_id)
+    company = CompanyProfile.build(name: name, website_url: website_url, contact_email: email, logo: logo, user: user)
+    company.valid?
+
+    if company.errors.empty?
+      company.save
+      @companies_created += 1
+      company.update(user: user)
+    else
+      @number_of_errors += 1
+      @errors << { line: line_number, data: row.to_s, errors: company.errors.full_messages }
+    end
   end
 
   def process_job(row, line_number)
+    title = row[1]&.strip
+    salary = row[2]&.strip
+    salary_currency = JobPosting.salary_currencies[row[3]&.downcase.strip]
+    salary_period = JobPosting.salary_periods[I18n.t("salary_period").invert[row[4]&.downcase.strip]]
+    work_arrangement = JobPosting.work_arrangements[I18n.t("work_arrangement").invert[row[5]&.downcase.strip]]
+    job_type_id = row[6]&.strip
+    job_location = row[7]&.strip
+    experience_level_id = row[8]&.strip
+    company_id = row[9]&.strip
+
+    company = CompanyProfile.find_by(id: company_id)
+    job_type = JobType.find_by(id: job_type_id)
+    experience_level = ExperienceLevel.find_by(id: experience_level_id)
+    job_post = JobPosting.build(title: title, salary: salary, salary_currency: salary_currency, salary_period: salary_period,
+                                work_arrangement: work_arrangement, description: "Default", job_location: job_location,
+                                company_profile: company, job_type: job_type, experience_level: experience_level)
+
+    if job_post.valid?
+      job_post.save
+      @jobs_created += 1
+    else
+      @number_of_errors += 1
+      @errors << { line: line_number, data: row.to_s, errors: job_post.errors.full_messages }
+    end
   end
 end
--- spec/models/import_service_spec.rb
+++ spec/models/import_service_spec.rb
@@ -48,9 +48,125 @@ RSpec.describe ImportService, type: :model do
     end
   end
 
-  describe '#process_user' do
+  describe '#process_company' do
     it 'success' do
-      
+      create(:user, email_address: 'emailteste@teste.com', id: 1)
+      create(:user, email_address: 'emailteste2@teste2.com', id: 2)
+      source = Rails.root.join('spec/support/files/import/company_text_test.txt')
+      file = ImportService.new(source)
+
+      file.process
+
+      expect(file.total_lines).to eq 2
+      expect(file.processed_lines).to eq 2
+      expect(file.number_of_errors).to eq 0
+      expect(file.users_created).to eq 0
+      expect(file.companies_created).to eq 2
+      expect(file.jobs_created).to eq 0
+      expect(CompanyProfile.last.name).to eq 'Empresa B'
+      expect(CompanyProfile.last.contact_email).to eq 'contato@empresa-b.com'
+      expect(CompanyProfile.last.website_url).to eq 'https://www.empresa-b.com'
+      expect(CompanyProfile.last.user.id).to eq 2
+      expect(CompanyProfile.first.user.id).to eq 1
+    end
+
+    it 'with errors on creation' do
+      create(:user, email_address: 'emailteste@teste.com', id: 1)
+      create(:user, email_address: 'emailteste2@teste2.com', id: 2)
+      source = Rails.root.join('spec/support/files/import/company_text_with_errors_test.txt')
+      file = ImportService.new(source)
+
+      file.process
+
+      expect(file.total_lines).to eq 3
+      expect(file.processed_lines).to eq 3
+      expect(file.number_of_errors).to eq 1
+      expect(file.users_created).to eq 0
+      expect(file.companies_created).to eq 2
+      expect(file.jobs_created).to eq 0
+      expect(CompanyProfile.last.name).to eq 'Empresa B'
+      expect(CompanyProfile.last.contact_email).to eq 'contato@empresa-b.com'
+      expect(CompanyProfile.last.website_url).to eq 'https://www.empresa-b.com'
+      expect(CompanyProfile.last.user.id).to eq 2
+      expect(CompanyProfile.first.user.id).to eq 1
+      expect(file.errors[0][:line]).to eq 3
+      expect(file.errors[0][:errors]).to match_array [ 'User é obrigatório(a)', 'Nome não pode ficar em branco', 'URL do Site não pode ficar em branco', 'Email de Contato não pode ficar em branco', 'Email de Contato não é válido', 'URL do Site deve ser um URL válido (começar com http:// ou https://, ter ponto separando o domínio e terminar com uma extensão válida de 2 a 6 caracteres)' ]
+    end
+  end
+
+  describe '#process_job' do
+    it 'success' do
+      user1 = create(:user, email_address: 'emailteste@teste.com', id: 1)
+      user2 = create(:user, email_address: 'emailteste2@teste2.com', id: 2)
+      user3 = create(:user, email_address: 'emailteste3@teste3.com', id: 3)
+      create(:company_profile, name: 'Company 1', contact_email: 'company1@email.com', id: 1, user: user1)
+      create(:company_profile, name: 'Company 2', contact_email: 'company2@email.com', id: 2, user: user2)
+      create(:company_profile, name: 'Company 3', contact_email: 'company3@email.com', id: 3, user: user3)
+      create(:job_type, id: 1, name: 'Teste 1')
+      create(:job_type, id: 2, name: 'Teste 2')
+      create(:job_type, id: 3, name: 'Teste 3')
+      create(:experience_level, id: 1, name: 'Teste 1')
+      create(:experience_level, id: 2, name: 'Teste 2')
+      create(:experience_level, id: 3, name: 'Teste 3')
+      source = Rails.root.join('spec/support/files/import/job_posting_text_test.txt')
+      file = ImportService.new(source)
+
+      file.process
+
+      expect(file.total_lines).to eq 3
+      expect(file.processed_lines).to eq 3
+      expect(file.number_of_errors).to eq 0
+      expect(file.users_created).to eq 0
+      expect(file.companies_created).to eq 0
+      expect(file.jobs_created).to eq 3
+      expect(JobPosting.last.title).to eq 'Gerente de Projetos'
+      expect(JobPosting.last.salary).to eq 8_000
+      expect(JobPosting.last.salary_currency).to eq 'brl'
+      expect(JobPosting.last.salary_period).to eq 'monthly'
+      expect(JobPosting.last.work_arrangement).to eq 'hybrid'
+      expect(JobPosting.last.job_location).to eq 'São Paulo'
+      expect(JobPosting.last.company_profile.id).to eq 3
+      expect(JobPosting.last.job_type.id).to eq 3
+      expect(JobPosting.last.experience_level.id).to eq 3
+      expect(JobPosting.first.company_profile.id).to eq 1
+    end
+
+    it 'with errors on creation' do
+      user1 = create(:user, email_address: 'emailteste@teste.com', id: 1)
+      user2 = create(:user, email_address: 'emailteste2@teste2.com', id: 2)
+      user3 = create(:user, email_address: 'emailteste3@teste3.com', id: 3)
+      create(:company_profile, name: 'Company 1', contact_email: 'company1@email.com', id: 1, user: user1)
+      create(:company_profile, name: 'Company 2', contact_email: 'company2@email.com', id: 2, user: user2)
+      create(:company_profile, name: 'Company 3', contact_email: 'company3@email.com', id: 3, user: user3)
+      create(:job_type, id: 1, name: 'Teste 1')
+      create(:job_type, id: 2, name: 'Teste 2')
+      create(:job_type, id: 3, name: 'Teste 3')
+      create(:experience_level, id: 1, name: 'Teste 1')
+      create(:experience_level, id: 2, name: 'Teste 2')
+      create(:experience_level, id: 3, name: 'Teste 3')
+      source = Rails.root.join('spec/support/files/import/job_posting_text_with_error_test.txt')
+      file = ImportService.new(source)
+
+      file.process
+
+      expect(file.total_lines).to eq 4
+      expect(file.processed_lines).to eq 4
+      expect(file.number_of_errors).to eq 1
+      expect(file.users_created).to eq 0
+      expect(file.companies_created).to eq 0
+      expect(file.jobs_created).to eq 3
+      expect(JobPosting.last.title).to eq 'Gerente de Projetos'
+      expect(JobPosting.last.salary).to eq 8_000
+      expect(JobPosting.last.salary_currency).to eq 'brl'
+      expect(JobPosting.last.salary_period).to eq 'monthly'
+      expect(JobPosting.last.work_arrangement).to eq 'hybrid'
+      expect(JobPosting.last.job_location).to eq 'São Paulo'
+      expect(JobPosting.last.company_profile.id).to eq 3
+      expect(JobPosting.last.job_type.id).to eq 3
+      expect(JobPosting.last.experience_level.id).to eq 3
+      expect(JobPosting.first.company_profile.id).to eq 1
+      expect(file.errors[0][:line]).to eq 4
+      expect(file.errors[0][:errors]).to match_array [ 'Company profile é obrigatório(a)', 'Job type é obrigatório(a)', 'Experience level é obrigatório(a)', 'Título não pode ficar em branco', 'Salário não pode ficar em branco', 'Moeda não pode ficar em branco', 'Período do salário não pode ficar em branco', 'Company profile não pode ficar em branco', 'Arranjo de trabalho não pode ficar em branco' ]
     end
   end
 end
